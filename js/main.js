b40dd9bdebcafc705dc6c21d30b5a5b88c7b03ec
abdelrhman_ali <abdelrahman.ali.sayed27@gmail.com>
2020-08-07 19:44:44 +0200

    creational commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+
+form.addEventListener('submit', e => {
+  /*  preventDefault prevent the form to submited until 
+  validate it.*/
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  // trim to remove the whitespaces
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+
+  if (usernameValue === '') {
+    setErrorFor(username, 'Username cannot be blank');
+  } else {
+    setSuccessFor(username);
+  }
+
+  if (emailValue === '') {
+    setErrorFor(email, 'Email cannot be blank');
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, 'Not a valid email');
+  }else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === '') {
+    setErrorFor(password, 'Password cannot be blank');
+  } else {
+    setSuccessFor(password);
+  }
+
+  if (password2Value === '') {
+    setErrorFor(password2, 'Password2 cannot be blank');
+  } else if (password2Value !== passwordValue) {
+    setErrorFor(password2, 'Passwords does not match');
+  } else {
+    setSuccessFor(password2);
+  }
+
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector('small');
+  formControl.className = 'form-control error';
+  small.innerText = message;
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+  const rgex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+  return rgex.test(email)
+}
